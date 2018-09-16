6c3e482e7714d136e7bf04941df9f292b36873f5
wqq1111 <707573606@qq.com>
2018-09-16 18:35:14 +0800

    apple

---
+++ js/demo.js
@@ -0,0 +1,31 @@
+$(function(){
+
+	//小屏下拉
+	let btn=$(".list");
+	let more=$(".head-more");
+	let line1=$(".line1");
+	let line2=$(".line2");
+
+	btn.click(function(){
+		more.slideToggle("slow");
+		line1.toggleClass("active");
+		line2.toggleClass("active1");
+	})
+
+	//小屏底部
+	let add=$(".add");
+	let down=$(".content-down");
+	let both=$(".footer2 ul li");
+	both.click(function(){
+		let i=$(this).index();
+		down.eq(i).slideToggle("slow");
+
+		add.eq(i).toggleClass('hot');
+	});
+
+
+
+	
+
+
+})
